eb9713fa9d329a72b56860aa041501230f2a8ccb
Stephany Mendes <56844567+SteScarpel@users.noreply.github.com>
2022-02-18 21:46:13 -0300

    Update SQL
    Criação da tabela Clientes

--- SQLQuery_CriacaoBanco.sql
+++ SQLQuery_CriacaoBanco.sql
@@ -4,7 +4,15 @@ create database banco_transacao
 use banco_transacao
 
 -- Criacao da tabela de Clientes
-
+create table Cliente (
+id int not null primary key,
+nome varchar(255),
+email varchar(255),
+data_de_cadastro datetime,
+pais_telefone varchar(5),
+ddd_telefone varchar(5),
+telefone varchar(10)
+);
 
 --Criacao da tabela de Transações-IN  
 
