26887dccf3d6a9314781f800a04b2df06ed82e41
Adriana <sijacinto@yahoo.com>
2022-02-19 00:41:39 -0300

    Add files via upload
    Atualização do script de criação do Banco de dados, com acréscimo de chave estrangeira.

--- SQLQuery_CriacaoBanco.sql
+++ SQLQuery_CriacaoBanco.sql
@@ -1,39 +1,35 @@
 -- Criação da dabase do banco 
 
-create database banco_transacao
-use banco_transacao
+create database banco_transacao;
+use banco_transacao;
 
 -- Criacao da tabela de Clientes
 create table Cliente (
 id int not null primary key,
 nome varchar(255) not null,
 email varchar(255),
-
-);
-
-create table Telefone (
-id_telefone int not null identity(1,1) primary key ,
-cliente_id int not null,
 data_de_cadastro datetime,
-pais_telefone varchar(5),
-ddd_telefone varchar(5),
-telefone varchar(10)
+telefone varchar(10),
 );
+
 
 --Criacao da tabela de Transações-IN  
 
 create table Transacoes_in (
 id int not null primary key, 
-cliente_id int not null, 
+cliente_id int not null FOREIGN KEY REFERENCES Cliente (id), 
 valor float, 
-data_transacao_in datetime not null
+data_transacao_in datetime not null,
+
 );
+
+
 
 -- Criacao da tabela de Transações-OUT
 
 create table Transacoes_out (
 id int not null primary key, 
-cliente_id int not null, 
+cliente_id int not null FOREIGN KEY REFERENCES Cliente (id), 
 valor float, 
-data_transacao_out datetime not null
+data_transacao_out datetime not null,
 );
