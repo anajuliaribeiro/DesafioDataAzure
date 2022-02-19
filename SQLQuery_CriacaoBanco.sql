fab0a2cc930bb3bdc04318ac2fdaf9f56afb7252
Stephany Mendes <56844567+SteScarpel@users.noreply.github.com>
2022-02-18 21:37:45 -0300

    Primeiro arquivo de SQl
    Script de criação do banco de dados

---
+++ SQLQuery_CriacaoBanco.sql
@@ -0,0 +1,12 @@
+-- Criação da dabase do banco 
+
+create database banco_transacao
+use banco_transacao
+
+-- Criacao da tabela de Clientes
+
+
+--Criacao da tabela de Transações-IN  
+
+
+-- Criacao da tabela de Transações-OUT
