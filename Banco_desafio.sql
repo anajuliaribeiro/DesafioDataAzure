672ec54bd9645c5d785c19a8fe3d9292f5822bba
Luana Miranda <ms.luana.mirand@gmail.com>
2022-02-23 20:34:59 -0300

    Upadate banco de dados

--- Banco_desafio.sql
+++ Banco_desafio.sql
@@ -1,15 +1,10 @@
--- Cria��o da dabase do banco 
+-- Criacao da dabase do banco 
 
 create database banco_transacao;
 use banco_transacao;
+go
 
-/*
-drop table Transacoes_in;
-drop table Transacoes_out;
-drop table Fraudes;
-drop table Telefones;
-drop table Clientes;
-*/
+
 -- Criacao da tabela de Clientes
 
 create table Clientes (
@@ -18,6 +13,7 @@ nome varchar(255) not null,
 email varchar(255),
 data_de_cadastro datetime
 );
+go
 
 --Criacao da tabela telefone
 
@@ -28,8 +24,14 @@ cod_pais varchar(5),
 ddd varchar(3),
 numero_telefone varchar(20)
 );
+go
 
---Criacao da tabela de Transa��es-IN  
+-- Criacao da constraint da foreign key 
+
+alter table Telefones add foreign key (cliente_id) references Clientes(id)
+go
+
+--Criacao da tabela de Transacoes-IN  
 
 create table Transacoes_in (
 id int not null primary key, 
@@ -37,9 +39,14 @@ cliente_id int not null,
 valor float, 
 data_transacao_in datetime not null
 );
+go
 
+-- Criacao da constraint da foreign key 
 
--- Criacao da tabela de Transa��es-OUT
+alter table Transacoes_in add foreign key (cliente_id) references Clientes(id)
+go
+
+-- Criacao da tabela de Transacoes-OUT
 
 create table Transacoes_out (
 id int not null primary key, 
@@ -47,21 +54,48 @@ cliente_id int not null,
 valor float, 
 data_transacao_out datetime not null
 );
+go
+
+-- Criacao da constraint da foreign key 
+
+alter table Transacoes_out add foreign key (cliente_id) references Clientes(id)
+go
 
 -- Criacao da tabela de Fraudes
 
-create table Fraudes(
+create table Fraudes (
 id int not null identity(1,1) primary key, 
 id_transacao int not null,
 tipo_transacao char(3) 
 );
+go 
 
+create table Fraudes_in (
+id int not null identity(1,1) primary key, 
+id_transacao int not null,
+tipo_transacao char(3) 
+);
+go 
+
+create table Fraudes_out (
+id int not null identity(1,1) primary key, 
+id_transacao int not null,
+tipo_transacao char(3) 
+);
+go
+
+-- Criacao da constraint da foreign key 
+alter table Fraudes add foreign key (id_transacao) references Transacoes_out(id)
+go
+select * from sys.foreign_key_columns
 
 
 select * from Clientes;
-
 select *from Transacoes_out;
 select *from Transacoes_in;
 select * from Telefones;
+select * from Fraudes;
+
+
 
 
